e21990b1eab2214a96688c574ae2c6d28ddaa1c9
Chorba-Exceed <chorba-exceed@gmail.com>
2019-12-19 10:55:10 +0300

    work

--- src/database/connectionDB.js
+++ src/database/connectionDB.js
@@ -5,5 +5,6 @@ module.exports = function () {
     console.log('Connect on TEST DB');
     return mongoose.connect('mongodb://localhost:27017/mongoapp_db_test', { useNewUrlParser: true, useUnifiedTopology: true });
   }
+  console.log('DB connect success');
   return mongoose.connect('mongodb+srv://vitaliy:vitaliy@cluster0-bkoze.mongodb.net/test?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB', { useNewUrlParser: true, useUnifiedTopology: true });
 };
