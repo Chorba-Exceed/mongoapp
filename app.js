6b09d594565112c5ad239cb93699142b2a04f392
Chorba-Exceed <chorba-exceed@gmail.com>
2019-12-19 12:13:51 +0300

    add Access-Control-Allow-Origin

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const cors = require('cors');
 
 const app = express();
 const bodyParser = require('body-parser');
@@ -21,7 +20,7 @@ app.use(bodyParser.json({
 }));
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://vitaliymongo.herokuapp.com/');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
