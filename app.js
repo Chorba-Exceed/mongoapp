fe211451e7302e97c947a877bfef269148dc3766
Chorba-Exceed <chorba.exceedteam@gmail.com>
2019-12-24 12:55:02 +0300

    add CORS Methods Headers

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json({
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, authorization, Content-Length, X-Requested-With');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, authorization, X-Requested-With');
   next();
 });
 
